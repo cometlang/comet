55ae91afc7f8d5a9b63ed323180e458a1d7f29da
Michael Malone <michael.j.b.malone@gmail.com>
2019-09-29 14:59:11 +1300

    Chapter 15 - Crafting an interpreter
    Adds the bones of the VM and start interpreting byte code.

---
+++ clox/vm.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include "vm.h"
+#include "debug.h"
+
+static VM vm;
+
+static void resetStack(void)
+{
+    vm.stackTop = vm.stack;
+}
+
+void initVM(void)
+{
+    resetStack();
+}
+
+void freeVM(void) {}
+
+void push(Value value)
+{
+    *vm.stackTop = value;
+    vm.stackTop++;
+}
+
+Value pop(void)
+{
+    vm.stackTop--;
+    return *vm.stackTop;
+}
+
+static InterpretResult run(void)
+{
+#define READ_BYTE() (*vm.ip++)
+#define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op)   \
+    do {                \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b);     \
+    } while (false)
+
+    for (;;)
+    {
+        uint8_t instruction;
+#if DEBUG_TRACE_EXECUTION
+        disassembleInstruction(vm.chunk, (int)(vm.ip - vm.chunk->code));
+        printf("          ");
+        for (Value *slot = vm.stack; slot < vm.stackTop; slot++)
+        {
+            printf("[ ");
+            printValue(*slot);
+            printf(" ]");
+        }
+        printf("\n");
+#endif
+        switch (instruction = READ_BYTE())
+        {
+            case OP_CONSTANT:
+            {
+                Value constant = READ_CONSTANT();
+                push(constant);
+                break;
+            }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE:   push(-pop()); break;
+            case OP_RETURN:
+            {
+                printValue(pop());
+                printf("\n");
+                return INTERPRET_OK;
+            }
+        }
+    }
+
+#undef BINARY_OP
+#undef READ_CONSTANT
+#undef READ_BYTE
+}
+
+InterpretResult interpret(Chunk *chunk)
+{
+    vm.chunk = chunk;
+    vm.ip = vm.chunk->code;
+    return run();
+}
---
+++ clox/vm.h
@@ -0,0 +1,31 @@
+#ifndef clox_vm_h
+#define clox_vm_h
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+    Chunk *chunk;
+    uint8_t *ip;
+    Value stack[STACK_MAX];
+    Value* stackTop;
+} VM;
+
+typedef enum {            
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR,
+} InterpretResult;
+
+void initVM(void);
+
+void freeVM(void);
+
+InterpretResult interpret(Chunk *chunk);
+
+void push(Value value);
+Value pop(void);
+
+#endif

c77ee436052220356aa51c10e0b75c96a0e18f94
Michael Malone <michael.j.b.malone@gmail.com>
2021-01-13 11:11:12 +1300

    Don't need to finish object definition twice
    In order for String to inherit from Object, Object's definition needs to be
    completed before String's definition is completed.  And String's
    definition needs to be completed before adding any methods, because
    functions and globals make use of String instances.

--- stdlib/_init.c
+++ stdlib/_init.c
@@ -5,7 +5,6 @@ void init_stdlib(void)
 {
     VALUE obj_klass = bootstrapNativeClass("Object", NULL, NULL);
     init_string(obj_klass);
-    completeNativeClassDefinition(obj_klass, "Object");
     init_object(obj_klass);
     init_functions();
     init_nil();

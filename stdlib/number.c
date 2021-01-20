4df000c20d2858d23e42a9aa7a983d3e511b5267
Michael Malone <michael.j.b.malone@gmail.com>
2021-01-21 09:55:21 +1300

    Rough job of implementing the Number class
    I say a rough job because it doesn't deal with Integer vs. Floating
    Point, but it will do Math when asked to.

--- stdlib/number.c
+++ stdlib/number.c
@@ -1,10 +1,14 @@
+#include <stdio.h>
 #include "cometlib.h"
+#include "comet_stdlib.h"
 
 typedef union {
   uint64_t bits64;
   uint32_t bits32[2];
   double num;
 } NumberData;
+
+static VALUE number_class;
 
 void *number_constructor()
 {
@@ -18,37 +22,62 @@ void number_destructor(void *data)
     FREE(NumberData, data);
 }
 
-VALUE number_to_string(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE number_to_string(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
 {
-    return copyString(vm, "number", 6);
+    NumberData *data = GET_NATIVE_INSTANCE_DATA(NumberData, self);
+#define TEMP_STRING_MAX_LEN 64
+    char temp[TEMP_STRING_MAX_LEN];
+    int length = snprintf(temp, TEMP_STRING_MAX_LEN, "%ld", (int64_t) data->num);
+    return copyString(vm, temp, length);
+#undef TEMP_STRING_MAX_LEN
 }
 
-VALUE number_operator_plus(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE number_operator_plus(VM *vm, VALUE self, int UNUSED(arg_count), VALUE *arguments)
 {
-    return NIL_VAL;
+    NumberData *lhs = GET_NATIVE_INSTANCE_DATA(NumberData, self);
+    NumberData *rhs = GET_NATIVE_INSTANCE_DATA(NumberData, arguments[0]);
+    VALUE result = OBJ_VAL(newInstance(vm, AS_CLASS(number_class)));
+    NumberData *result_data = GET_NATIVE_INSTANCE_DATA(NumberData, result);
+    result_data->num = lhs->num + rhs->num;
+    return result;
 }
 
-VALUE number_operator_minus(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE number_operator_minus(VM *vm, VALUE self, int UNUSED(arg_count), VALUE *arguments)
 {
-    return NIL_VAL;
+    NumberData *lhs = GET_NATIVE_INSTANCE_DATA(NumberData, self);
+    NumberData *rhs = GET_NATIVE_INSTANCE_DATA(NumberData, arguments[0]);
+    VALUE result = OBJ_VAL(newInstance(vm, AS_CLASS(number_class)));
+    NumberData *result_data = GET_NATIVE_INSTANCE_DATA(NumberData, result);
+    result_data->num = lhs->num - rhs->num;
+    return result;
 }
 
-VALUE number_operator_divide(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE number_operator_divide(VM *vm, VALUE self, int UNUSED(arg_count), VALUE *arguments)
 {
-    return NIL_VAL;
+    NumberData *lhs = GET_NATIVE_INSTANCE_DATA(NumberData, self);
+    NumberData *rhs = GET_NATIVE_INSTANCE_DATA(NumberData, arguments[0]);
+    VALUE result = OBJ_VAL(newInstance(vm, AS_CLASS(number_class)));
+    NumberData *result_data = GET_NATIVE_INSTANCE_DATA(NumberData, result);
+    result_data->num = lhs->num / rhs->num;
+    return result;
 }
 
-VALUE number_operator_multiply(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE number_operator_multiply(VM *vm, VALUE self, int UNUSED(arg_count), VALUE *arguments)
 {
-    return NIL_VAL;
+    NumberData *lhs = GET_NATIVE_INSTANCE_DATA(NumberData, self);
+    NumberData *rhs = GET_NATIVE_INSTANCE_DATA(NumberData, arguments[0]);
+    VALUE result = OBJ_VAL(newInstance(vm, AS_CLASS(number_class)));
+    NumberData *result_data = GET_NATIVE_INSTANCE_DATA(NumberData, result);
+    result_data->num = lhs->num * rhs->num;
+    return result;
 }
 
 void init_number(VM *vm)
 {
-    VALUE klass = defineNativeClass(vm, "Number", &number_constructor, &number_destructor, NULL);
-    defineNativeMethod(vm, klass, &number_to_string, "to_string", false);
-    defineNativeOperator(vm, klass, &number_operator_plus, OPERATOR_PLUS);
-    defineNativeOperator(vm, klass, &number_operator_minus, OPERATOR_MINUS);
-    defineNativeOperator(vm, klass, &number_operator_divide, OPERATOR_DIVISION);
-    defineNativeOperator(vm, klass, &number_operator_multiply, OPERATOR_MULTIPLICATION);
+    number_class = defineNativeClass(vm, "Number", &number_constructor, &number_destructor, NULL);
+    defineNativeMethod(vm, number_class, &number_to_string, "to_string", false);
+    defineNativeOperator(vm, number_class, &number_operator_plus, OPERATOR_PLUS);
+    defineNativeOperator(vm, number_class, &number_operator_minus, OPERATOR_MINUS);
+    defineNativeOperator(vm, number_class, &number_operator_divide, OPERATOR_DIVISION);
+    defineNativeOperator(vm, number_class, &number_operator_multiply, OPERATOR_MULTIPLICATION);
 }

781d06d2b9184c648bd9fc479a4b980873ea5857
Michael Malone <michael.j.b.malone@gmail.com>
2021-03-01 18:14:24 +1300

    Add a mutex class
    This is a very basic implementation and doesn't do all that tedious
    error checking that I'm supposed to do.  Oh well.

--- stdlib/thread_synchronisation.c
+++ stdlib/thread_synchronisation.c
@@ -4,12 +4,14 @@
 
 typedef struct {
     pthread_cond_t cond_var;
+    pthread_mutex_t lock;
 } CondVarData;
 
 void *cond_var_constructor(void)
 {
     CondVarData *data = ALLOCATE(CondVarData, 1);
     pthread_cond_init(&data->cond_var, NULL);
+    pthread_mutex_init(&data->lock, NULL);
     return data;
 }
 
@@ -17,27 +19,76 @@ void cond_var_destructor(void *data)
 {
     CondVarData *cond_var = (CondVarData *)data;
     pthread_cond_destroy(&cond_var->cond_var);
+    pthread_mutex_destroy(&cond_var->lock);
     FREE(CondVarData, data);
 }
 
-VALUE cond_var_signal_one(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE cond_var_signal_one(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
 {
+    CondVarData *data = GET_NATIVE_INSTANCE_DATA(CondVarData, self);
+    pthread_cond_signal(&data->cond_var);
     return NIL_VAL;
 }
 
-VALUE cond_var_signal_all(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE cond_var_signal_all(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
 {
+    CondVarData *data = GET_NATIVE_INSTANCE_DATA(CondVarData, self);
+    pthread_cond_broadcast(&data->cond_var);
     return NIL_VAL;
 }
 
-VALUE cond_var_wait(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+VALUE cond_var_wait(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
 {
+    CondVarData *data = GET_NATIVE_INSTANCE_DATA(CondVarData, self);
+    pthread_cond_wait(&data->cond_var, &data->lock);
     return NIL_VAL;
 }
 
 VALUE cond_var_timed_wait(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
 {
+    CondVarData UNUSED(*data) = GET_NATIVE_INSTANCE_DATA(CondVarData, self);
+    // pthread_cond_timedwait(&data->cond_var, &data->lock);
     return NIL_VAL;
+}
+
+
+typedef struct {
+    pthread_mutex_t mutex;
+} MutexData;
+
+void *mutex_constructor(void)
+{
+    MutexData *data = ALLOCATE(MutexData, 1);
+    pthread_mutex_init(&data->mutex, NULL);
+    return data;
+}
+
+void mutex_destructor(void *data)
+{
+    pthread_mutex_destroy(&((MutexData *)data)->mutex);
+    FREE(MutexData, data);
+}
+
+VALUE mutex_unlock(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
+{
+    MutexData *data = GET_NATIVE_INSTANCE_DATA(MutexData, self);
+    pthread_mutex_unlock(&data->mutex);
+    return NIL_VAL;
+}
+
+VALUE mutex_lock(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
+{
+    MutexData *data = GET_NATIVE_INSTANCE_DATA(MutexData, self);
+    pthread_mutex_lock(&data->mutex);
+    return NIL_VAL;
+}
+
+VALUE mutex_try_lock(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
+{
+    MutexData *data = GET_NATIVE_INSTANCE_DATA(MutexData, self);
+    if (pthread_mutex_trylock(&data->mutex) == 0)
+        return TRUE_VAL;
+    return FALSE_VAL;
 }
 
 void init_thread_sync(VM *vm)
@@ -48,4 +99,9 @@ void init_thread_sync(VM *vm)
     defineNativeMethod(vm, cond_var_class, &cond_var_signal_all, "signal_all", 0, false);
     defineNativeMethod(vm, cond_var_class, &cond_var_wait, "wait", 0, false);
     defineNativeMethod(vm, cond_var_class, &cond_var_timed_wait, "timed_wait", 0, false);
+
+    VALUE mutex_class = defineNativeClass(vm, "Mutex", &mutex_constructor, &mutex_destructor, NULL, CLS_MUTEX);
+    defineNativeMethod(vm, mutex_class, &mutex_lock, "lock", 0, false);
+    defineNativeMethod(vm, mutex_class, &mutex_unlock, "unlock", 0, false);
+    defineNativeMethod(vm, mutex_class, &mutex_try_lock, "try_lock", 0, false);
 }

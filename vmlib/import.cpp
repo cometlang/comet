8bff3e51c0a5c5427970897155d6976c195eceb2
Michael Malone <michael.j.b.malone@gmail.com>
2021-08-28 13:26:45 +1200

    Improved import string handling
    Using c++ it is much clearer what I'm attempting to do, plus it's
    handily already cross-platform.

--- vmlib/import.cpp
+++ vmlib/import.cpp
@@ -9,45 +9,32 @@
 #endif
 
 #include <string>
+#include <string_view>
+#include <filesystem>
+
+#include <iostream>
 
 extern "C" {
 
 #include "import.h"
+#include "comet.h"
 #include "common.h"
 #include "compiler.h"
-#include "comet.h"
+
+using namespace std;
+
+constexpr string_view file_extenstion(".cmt");
 
 #define EXTENSION_MAX_STRLEN 4
 
-
-#ifdef WIN32
-static char* dirname(char* filename)
+ObjModule *import_from_file(VM *vm, const char *relative_to_filename, Value to_import)
 {
-    return filename;
-}
-#endif
+    const char *to_import_path = string_get_cstr(to_import);
+    string current_dir = filesystem::path(relative_to_filename).parent_path();
+    filesystem::path candidate = filesystem::path(current_dir) / filesystem::path(string(to_import_path) + string(file_extenstion));
+    filesystem::path absolute_path = filesystem::canonical(candidate);
 
-ObjModule *import_from_file(VM *vm, const char *filename, Value import_path)
-{
-    int filename_len = strlen(filename);
-    const char *path = string_get_cstr(import_path);
-    int path_len = strlen(path);
-    int length = path_len + filename_len + EXTENSION_MAX_STRLEN + 1;
-    char *candidate = (char *) malloc(sizeof(char) * length);
-    if (candidate == NULL)
-    {
-        runtimeError(vm, "Out of memory");
-        return NULL;
-    }
-    char *dir = dirname((char *)filename);
-    int dir_len = strlen(dir);
-    memcpy(candidate, dir, dir_len);
-    candidate[dir_len] = '/';
-    memcpy(&candidate[dir_len + 1], &path[1], path_len - 2);
-    memcpy(&candidate[dir_len + 1 + path_len - 2], ".cmt", 4);
-    candidate[dir_len + 1 + path_len - 2 + 4] = '\0';
-
-    char *full_path = realpath(candidate, NULL);
+    const char *full_path = absolute_path.c_str();
     ObjModule *module = NULL;
     Value full_path_val = copyString(vm, full_path, strlen(full_path));
     push(vm, full_path_val);
@@ -66,9 +53,6 @@ ObjModule *import_from_file(VM *vm, const char *filename, Value import_path)
         }
     }
     pop(vm);
-
-    free(candidate);
-    free(full_path);
 
     return module;
 }

cfdcdb4bf39deef6e6677a62962de9b53b1f4af5
Michael Malone <michael.j.b.malone@gmail.com>
2023-04-04 06:29:08 +1200

    Add duration accessors and to_string
    Add a bunch of fields that represent the duration in different units.

--- stdlib/duration.cpp
+++ stdlib/duration.cpp
@@ -1,4 +1,6 @@
 #include <chrono>
+#include <iostream>
+#include <sstream>
 #include <date/date.h>
 #include "comet.h"
 #include "cometlib.h"
@@ -16,6 +18,21 @@ std::chrono::nanoseconds duration_get_value(VALUE duration)
 {
     DurationData *data = GET_NATIVE_INSTANCE_DATA(DurationData, duration);
     return data->duration;
+}
+
+static void set_duration_properties(VM *vm, VALUE self, std::chrono::nanoseconds& value)
+{
+    setNativeProperty(vm, self, "nanoseconds", create_number(vm, value.count()));
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(value);
+    setNativeProperty(vm, self, "milliseconds", create_number(vm, milliseconds.count()));
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(value);
+    setNativeProperty(vm, self, "seconds", create_number(vm, seconds.count()));
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(value);
+    setNativeProperty(vm, self, "minutes", create_number(vm, minutes.count()));
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(value);
+    setNativeProperty(vm, self, "hours", create_number(vm, hours.count()));
+    auto days = std::chrono::duration_cast<std::chrono::days>(value);
+    setNativeProperty(vm, self, "days", create_number(vm, days.count()));
 }
 
 extern "C" {
@@ -52,6 +69,7 @@ static VALUE duration_init(VM UNUSED(*vm), VALUE self, int arg_count, VALUE* arg
     }
     DurationData *data = GET_NATIVE_INSTANCE_DATA(DurationData, self);
     data->duration = value;
+    set_duration_properties(vm, self, value);
     return NIL_VAL;
 }
 
@@ -90,7 +108,7 @@ static VALUE duration_from_milliseconds(VM* vm, VALUE self, int UNUSED(arg_count
     return duration_create(vm, std::chrono::milliseconds((int64_t)number_get_value(arguments[0])).count());
 }
 
-static VALUE duration_operator_plus(VM UNUSED(*vm), VALUE UNUSED(self), int UNUSED(arg_count), VALUE UNUSED(*arguments))
+static VALUE duration_operator_plus(VM *vm, VALUE self, int UNUSED(arg_count), VALUE *arguments)
 {
     DurationData *data = GET_NATIVE_INSTANCE_DATA(DurationData, OBJ_VAL(self));
     if (IS_INSTANCE_OF_STDLIB_TYPE(arguments[0], CLS_DATETIME)) {
@@ -106,11 +124,21 @@ static VALUE duration_operator_plus(VM UNUSED(*vm), VALUE UNUSED(self), int UNUS
     return NIL_VAL;
 }
 
+static VALUE duration_to_string(VM UNUSED(*vm), VALUE self, int UNUSED(arg_count), VALUE UNUSED(*arguments))
+{
+    DurationData *data = GET_NATIVE_INSTANCE_DATA(DurationData, OBJ_VAL(self));
+    std::ostringstream out;
+    out << data->duration;
+    auto output = out.str();
+    return copyString(vm, output.c_str(), output.length());
+}
+
 VALUE duration_create(VM *vm, const int64_t count)
 {
     VALUE result = OBJ_VAL(newInstance(vm, AS_CLASS(duration_class)));
     DurationData *result_data = GET_NATIVE_INSTANCE_DATA(DurationData, result);
     result_data->duration = std::chrono::nanoseconds(count);
+    set_duration_properties(vm, result, result_data->duration);
     return result;
 }
 
@@ -119,6 +147,7 @@ void init_duration(VM *vm)
     duration_class = defineNativeClass(vm, "Duration", NULL, NULL, NULL, NULL, CLS_DURATION, sizeof(DurationData), false);
 
     defineNativeMethod(vm, duration_class, &duration_init, "init", 0, false);
+    defineNativeMethod(vm, duration_class, &duration_to_string, "to_string", 0, false);
 
     defineNativeMethod(vm, duration_class, &duration_from_years, "from_years", 1, true);
     defineNativeMethod(vm, duration_class, &duration_from_months, "from_months", 1, true);

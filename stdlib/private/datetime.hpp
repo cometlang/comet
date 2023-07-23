553f11fa7e4de51432e497d58f50e3bfe5171915
Michael Malone <michael.j.b.malone@gmail.com>
2023-07-23 19:58:24 +1200

    Fixed unix datetime compilation
    Switching between OSes is a painful way to do development.  I really
    wish people would just implememnt the standard >:/

--- stdlib/private/datetime.hpp
+++ stdlib/private/datetime.hpp
@@ -3,7 +3,8 @@
 #include <chrono>
 
 #ifndef WIN32
-#include "date.h"
+#include "date/date.h"
+#include "date/tz.h"
 #endif
 #include "comet.h"
 
@@ -11,7 +12,7 @@ extern "C" {
 #ifdef WIN32
     VALUE create_datetime(VM *vm, std::chrono::time_point<std::chrono::local_t, std::chrono::nanoseconds> time_point, const std::chrono::time_zone *time_zone);
 #else
-    VALUE create_datetime(VM* vm, std::chrono::time_point<date::local_t, std::chrono::nanoseconds> time_point, const std::chrono::time_zone* time_zone);
+    VALUE create_datetime(VM* vm, std::chrono::time_point<date::local_t, std::chrono::nanoseconds> time_point, const date::time_zone* time_zone);
 #endif
 }
 
